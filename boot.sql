84c963251f9f9af4d4d09f8774d784e025bef82c
祁玉 <924120316@qq.com>
2018-03-29 22:24:59 +0800

    sql初始化

--- boot.sql
+++ boot.sql
@@ -2,8 +2,8 @@
 /*用户表*/
 CREATE TABLE `user` (
   `uid` int(10) NOT NULL AUTO_INCREMENT,
-  `uname` varchar(255) DEFAULT NULL,
-  `birth` date DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `birth` date NOT NULL DEFAULT '0000-00-00' ,
   KEY `uid` (`uid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8
 
