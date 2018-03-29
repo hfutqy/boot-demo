7aaa856f8959020f84f225e1f484342fd4b27ea9
祁玉 <924120316@qq.com>
2018-03-29 21:39:24 +0800

    Initial commit

---
+++ src/main/java/com/qiyu/boot/service/IUserService.java
@@ -0,0 +1,18 @@
+package com.qiyu.boot.service;
+
+import com.qiyu.boot.entity.User;
+
+import java.util.List;
+
+/**
+ * @author qiyu
+ * @date 2017-02-25 09:16
+ * @since 1.8
+ * @version 1.0.0
+ */
+public interface IUserService {
+
+    String insert(User user);
+
+    List<User> getList();
+}
