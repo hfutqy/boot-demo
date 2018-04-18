0f5e754010cfc2d939ca3f8d0425ec1608ef0b09
祁玉 <924120316@qq.com>
2018-04-18 22:12:35 +0800

    常量类转接口

--- src/main/java/com/qiyu/boot/config/Constants.java
+++ src/main/java/com/qiyu/boot/config/Constants.java
@@ -7,20 +7,20 @@ package com.qiyu.boot.config;
  * @version 1.0.0
  * @since 1.8
  **/
-public class Constants {
+public interface Constants {
 
     /**
      * 默认年月日格式化
      */
-    public static final String DATE_DEFAULT_FORMAT = "YYYY-MM-DD";
+    String DATE_DEFAULT_FORMAT = "YYYY-MM-DD";
 
     /**
      * 默认日期时间格式化
      */
-    public static final String DATETIME_DEFAULT_FORMAT = "YYYY-MM-DD HH:mm:ss";
+    String DATETIME_DEFAULT_FORMAT = "YYYY-MM-DD HH:mm:ss";
 
     /**
      * 默认时间格式化
      */
-    public static final String TIME_DEFAULT_FORMAT = "HH:mm:ss";
+    String TIME_DEFAULT_FORMAT = "HH:mm:ss";
 }
